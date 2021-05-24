451edc26b092bc90fba829f0386943961c5e8810
aeae_1@naver.com <aeae_1@naver.com>
2021-05-24 14:09:43 +0900

    Section 4: 스프링 컨테이너와 스프링 빈
    - beanfind: 빈 조회
    - xml 설정
    - beandefinition : 빈 설정 메타 정보

---
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,22 @@
+package hello.core.xml;
+
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
